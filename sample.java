b6b586122dd7a3620e6cee4ccf978746a15e5e05
unknown <mitul.parmar@PDC2A-2K2D7BS.dir.svc.accenture.com>
2015-09-23 17:40:02 +0530

    Second Commit

--- sample.java
+++ sample.java
@@ -1,6 +1,6 @@
 public class sample {
   public static void main(String[] args) {
-      String[] data = { "\u250C\u2500\u2500\u2500\u2500\u2500\u2510", 
+     String[] data = { "\u250C\u2500\u2500\u2500\u2500\u2500\u2510", 
               "\u2502Hello\u2502",
 	              "\u2514\u2500\u2500\u2500\u2500\u2500\u2518" };
 		          for (String s : data) {
